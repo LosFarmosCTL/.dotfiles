b6232012cbc53ed1c07117105721c85255119129
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2022-04-30 20:31:36 +0200

    migrate from ts_utils to vim.treesitter.query for get_node_text

--- nvim/lua/plugins/context_vt.lua
+++ nvim/lua/plugins/context_vt.lua
@@ -2,8 +2,6 @@
 require("nvim_context_vt").setup({
     min_rows = 20,
     custom_parser = function(node, ft, opts)
-        local ts_utils = require("nvim-treesitter.ts_utils")
-
-        return "--> " .. ts_utils.get_node_text(node)[1]
+        return "--> " .. vim.split(vim.treesitter.query.get_node_text(node, 0), "\n")[1]
     end,
 })
