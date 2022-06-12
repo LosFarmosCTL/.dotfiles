71a96eabba31b780bcfc0c37c8f18880c4f90000
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2022-06-12 14:23:02 +0200

    feat(nvim/lspconfig): add tsserver

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,4 +1,4 @@
-local servers = { "clangd" }
+local servers = { "clangd", "tsserver" }
 local cmp_capabilities = require("cmp_nvim_lsp").update_capabilities(
     vim.lsp.protocol.make_client_capabilities()
 )
