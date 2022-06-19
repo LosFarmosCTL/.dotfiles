6d81718ee75fca55904f3adfb7eb3b9c2d4aed3b
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2022-06-19 19:55:56 +0200

    fix(nvim/lspconfig): use utf-16 offset encoding

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -2,6 +2,7 @@ local servers = { "clangd", "tsserver" }
 local cmp_capabilities = require("cmp_nvim_lsp").update_capabilities(
     vim.lsp.protocol.make_client_capabilities()
 )
+cmp_capabilities.offsetEncoding = { 'utf-16' }
 
 for _, lsp in pairs(servers) do
     require("lspconfig")[lsp].setup({
