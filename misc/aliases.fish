268d91c571fbe497ab5267fc234bb67681359465
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2022-06-25 17:38:23 +0200

    fix(misc/aliases): enclose c2 compile abbreviation in quotation marks to include &&

--- misc/aliases.fish
+++ misc/aliases.fish
@@ -4,4 +4,4 @@ abbr -ag c clear
 abbr -ag cat bat
 
 # compile command for chatterino2 https://github.com/chatterino/chatterino2
-abbr -ag c2make cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_PRECOMPILED_HEADERS=Off -DCMAKE_EXPORT_COMPILE_COMMANDS=YES .. && make -j5
+abbr -ag c2make 'cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_PRECOMPILED_HEADERS=Off -DCMAKE_EXPORT_COMPILE_COMMANDS=YES .. && make -j5'
