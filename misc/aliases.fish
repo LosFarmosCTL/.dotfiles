a8041011d71fd66b58874f446207eb4fb1ad7a36
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2022-06-25 17:33:37 +0200

    feat(misc/aliases): add c2make abbreviation to compile chatterino2

--- misc/aliases.fish
+++ misc/aliases.fish
@@ -2,3 +2,6 @@ abbr -ag ls ls -AFhlO
 abbr -ag c clear
 
 abbr -ag cat bat
+
+# compile command for chatterino2 https://github.com/chatterino/chatterino2
+abbr -ag c2make cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_PRECOMPILED_HEADERS=Off -DCMAKE_EXPORT_COMPILE_COMMANDS=YES .. && make -j5
