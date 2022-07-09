f0a87fe04223505739690404d57e969a9b2e1dcf
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2022-07-09 20:37:05 +0200

    feat(nvim/lspconfig): allow for server-specific arguments to be passed during LSP setup

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,12 +1,20 @@
-local servers = { "clangd", "tsserver" }
+local servers =
+    { { server = "clangd", setup = {} }, { server = "tsserver", setup = {} } }
+
 local cmp_capabilities = require("cmp_nvim_lsp").update_capabilities(
     vim.lsp.protocol.make_client_capabilities()
 )
-cmp_capabilities.offsetEncoding = { 'utf-16' }
+cmp_capabilities.offsetEncoding = { "utf-16" }
 
-for _, lsp in pairs(servers) do
-    require("lspconfig")[lsp].setup({
+for _, lsp in ipairs(servers) do
+    local setup = {
         on_attach = require("keymap").lsp.setup,
         capabilities = cmp_capabilities,
-    })
+    }
+    -- merge in LSP specific settings
+    for k, v in pairs(lsp.setup) do
+        setup[k] = v
+    end
+
+    require("lspconfig")[lsp.server].setup(setup)
 end
