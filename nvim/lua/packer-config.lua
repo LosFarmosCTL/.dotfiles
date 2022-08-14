e3d2434c7f7b9a7df58dfadc6a77ee43b0ba13cc
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2022-08-14 20:41:57 +0200

    chore(nvim/plugins): change plugin category name from essential to core

--- nvim/lua/packer-config.lua
+++ nvim/lua/packer-config.lua
@@ -13,7 +13,7 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 require("packer").startup(function(use)
-    --                                      ESSENTIAL                               {{{
+    --                                      CORE                               {{{
     -----------------------------------------------------------------------------------
     use("wbthomason/packer.nvim")
     use("lewis6991/impatient.nvim")
