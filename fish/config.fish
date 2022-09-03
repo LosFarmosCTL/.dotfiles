1b492b1ba44654040ec09d23bd3ab89602bbdbb3
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2022-09-03 22:59:56 +0200

    fix(fish): source fish config files for scripts as well

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,11 @@
+set -l DOTFILES ~/.dotfiles
+set -l config_files $DOTFILES/**.fish
+
+for config_file in (string match -vr 'config.fish$' $config_files)
+    source $config_file
+end
+
 if status is-interactive
-    set -l DOTFILES ~/.dotfiles
-    set -l config_files $DOTFILES/**.fish
-
-    for config_file in (string match -vr 'config.fish$' $config_files)
-        source $config_file
-    end
-
     # Set the cursor shapes for the different vi modes.
     set fish_cursor_default     block      blink
     set fish_cursor_insert      line       blink
