9aeef8a1b164bd3c4b96719acc09e6d7b13d1d36
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2022-09-04 00:11:50 +0200

    refactor(nvim/keymap): dont redefine local map function

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -95,7 +95,7 @@ return {
         setup = function(bufnr)
             local gs = package.loaded.gitsigns
 
-            local function map(mode, l, r, opts)
+            function map(mode, l, r, opts)
                 opts = opts or {}
                 opts.buffer = bufnr
                 vim.keymap.set(mode, l, r, opts)
