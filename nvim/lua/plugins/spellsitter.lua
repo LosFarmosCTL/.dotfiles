a365359a9cf1e6b2d5b8a35e255ef81ea4431566
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2022-09-04 13:45:20 +0200

    fix(neovim/spellsitter): fix autocmd disabling spellsitter for outline windows

--- nvim/lua/plugins/spellsitter.lua
+++ nvim/lua/plugins/spellsitter.lua
@@ -3,7 +3,7 @@ require("spellsitter").setup()
 local disable_spellsitter =
     vim.api.nvim_create_augroup("disable_spellsitter", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
-    patter = { "Outline" },
+    pattern = { "Outline" },
     command = "setlocal nospell",
     group = disable_spellsitter,
 })
