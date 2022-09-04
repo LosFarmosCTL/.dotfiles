40253d4c779e810a32ca70ba094d6b59beb74bd9
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2022-09-04 21:28:09 +0200

    fix(nvim/visual): set better looking highlights for tokyonight colorscheme

--- nvim/lua/plugins/illuminate.lua
+++ nvim/lua/plugins/illuminate.lua
@@ -2,6 +2,6 @@ require("illuminate").configure({
     delay = 500,
 })
 
-vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#384047", })
-vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#384047" })
-vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#384047" })
+vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#34364c", })
+vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#34364c" })
+vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#34364c" })
--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -2,3 +2,6 @@ vim.g.tokyonight_style = "night"
 vim.g.tokyonight_sidebars = { "packer", "Outline" }
 
 vim.cmd [[colorscheme tokyonight]]
+
+vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#283B4D" })
+vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#3C2C3C", fg = "#4d384d" })
