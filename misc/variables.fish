f8f7188d3e45a5d2fcf2cd95bc538b3ddeb2d229
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2022-09-28 15:35:42 +0200

    fix(misc/aliases): clean up PATH variables

--- misc/variables.fish
+++ misc/variables.fish
@@ -1,9 +1,9 @@
 # Append qt installation to path, for cmake to find it
-set PATH /usr/local/opt/qt@5/bin/ $PATH
+set PATH /usr/local/opt/qt@5/bin $PATH
 
 # Append LLVM installation to path
 # Homebrew does not link it to prevent overwriting apples built-in version, but it is needed for clangd
-set PATH /usr/local/opt/llvm/bin/ $PATH
+set PATH /usr/local/opt/llvm/bin $PATH
 
 # Sets the upload URL for haste-client to Leppunen's instance
 set -x HASTE_SERVER https://paste.ivr.fi
