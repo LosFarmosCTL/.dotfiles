90df22af882f3c368f661ab91b57fcdb62c9b901
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2022-09-28 15:32:20 +0200

    chore(nvim/plugins/copilot): update to new node@16 version

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,4 +1,4 @@
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 vim.g.copilot_tab_fallback = ""
-vim.g.copilot_node_command = "/usr/local/Cellar/node@16/16.17.0/bin/node"
+vim.g.copilot_node_command = "/usr/local/Cellar/node@16/16.17.1/bin/node"
