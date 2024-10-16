0d1b6fa26e623b3dc2a341a18a1c52c6118d0995
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2024-10-16 17:50:42 +0200

    fix(fish): use new vi mode activation method

--- fish/config.fish
+++ fish/config.fish
@@ -8,13 +8,7 @@ for config_file in (string match -vr 'config.fish$' $config_files)
 end
 
 if status is-interactive
-    # Set the cursor shapes for the different vi modes.
-    set fish_cursor_default     block      blink
-    set fish_cursor_insert      line       blink
-    set fish_cursor_replace_one underscore blink
-    set fish_cursor_visual      block
-
-    set fish_vi_force_cursor
+    fish_vi_key_bindings
 
     set fish_greeting
 
