a2fb928ef2ee84e4ffd1298b5b3c4fcc18e8079b
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2024-10-16 18:04:19 +0200

    chore(nvim): use gray background color for theme

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -8,5 +8,17 @@ return {
       -- You can configure highlights by doing something like:
       -- vim.cmd.hi 'Comment gui=none'
     end,
+    opts = {
+      -- transparent = true,
+      on_colors = function(colors)
+        colors.bg = '#1e1e1e'
+        colors.bg_dark = '#161616'
+        colors.bg_float = '#161616'
+        colors.bg_popup = '#161616'
+        colors.bg_sidebar = '#161616'
+        colors.bg_statusline = '#161616'
+        colors.bg_highlight = '#282828'
+      end,
+    },
   },
 }
