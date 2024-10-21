8e73b050884a331d79fc9f86bec49b5a44ee0d8a
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2024-10-21 16:55:20 +0200

    feat(nvim): use mini.notify as notifiaction handler

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -30,6 +30,13 @@ return {
         '.git',
         'Makefile',
       }
+
+      require('mini.notify').setup {
+        lsp_progress = {
+          enable = false,
+        },
+      }
+      vim.notify = require('mini.notify').make_notify()
     end,
   },
 }
