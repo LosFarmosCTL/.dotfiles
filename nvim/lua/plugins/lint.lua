eac75f7f07761d1269f16a47b0453d06e94c0ed1
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2024-11-29 01:26:12 +0100

    feat(nvim): add eslint to linter configuration

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -6,6 +6,8 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         swift = { 'swiftlint' },
+        typescript = { 'eslint' },
+        javascript = { 'eslint' },
       }
 
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
