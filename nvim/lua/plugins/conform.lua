e64388de7f60952038af5ef75781416454e1fab5
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2024-12-07 04:07:33 +0100

    feat(nvim): add swift-format to conform config

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -27,6 +27,7 @@ return {
         end
       end,
       formatters_by_ft = {
+        swift = { 'swift-format' },
         lua = { 'stylua' },
         python = { 'ruff_format', 'ruff_fix' },
         css = { 'prettierd', 'prettier', stop_after_first = true },
