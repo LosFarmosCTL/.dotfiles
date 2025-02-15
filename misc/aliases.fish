224a2eaa8b2885733f1cf599026d5e9942830cee
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-02-15 04:46:00 +0100

    feat(fish): add caffeinate alias

--- misc/aliases.fish
+++ misc/aliases.fish
@@ -1,5 +1,7 @@
 abbr -ag ls ls -AFhlO
 abbr -ag c clear
 
+abbr -ag caffeinate caffeinate -d -i -u -s
+
 # compile command for chatterino2 https://github.com/chatterino/chatterino2
 abbr -ag c2make 'cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_PRECOMPILED_HEADERS=Off -DCMAKE_EXPORT_COMPILE_COMMANDS=YES .. && make -j5'
