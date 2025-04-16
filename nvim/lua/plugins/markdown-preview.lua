6dd2ace0393e6ebf1f24ae452dc94450b1f982ad
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-04-17 01:42:53 +0200

    fix(nvim): remove live-preview.nvim in favor of peek.nvim

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -9,7 +9,17 @@ return {
     },
   },
   {
-    'brianhuster/live-preview.nvim',
-    cmd = 'LivePreview',
+    'toppair/peek.nvim',
+    event = { 'VeryLazy' },
+    build = 'deno task --quiet build:fast',
+    config = function()
+      require('peek').setup()
+      vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
+      vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+    end,
+    cmd = {
+      'PeekOpen',
+      'PeekClose',
+    },
   },
 }
