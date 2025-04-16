5f68d02d9c7702370c69ad95d47b7a411994c871
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-04-16 13:23:31 +0200

    feat(nvim): add nvim-colorizer for previewing colors in css etc

---
+++ nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'norcalli/nvim-colorizer.lua',
+    event = { 'VeryLazy' },
+    opts = {
+      css = { rgb_fn = true, hsl_fn = true, css = true, css_fn = true, RRGGBBAA = true },
+    },
+  },
+}
