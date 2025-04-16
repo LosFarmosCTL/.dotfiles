e66b2680ec3f079fd57b28406c436549628df2ce
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-04-17 01:56:40 +0200

    chore(nvim): remove leftover comment

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -4,9 +4,6 @@ return {
     priority = 1000,
     init = function()
       vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      -- vim.cmd.hi 'Comment gui=none'
     end,
   },
 }
