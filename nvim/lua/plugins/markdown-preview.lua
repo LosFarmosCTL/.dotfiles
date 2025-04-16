c43145133e510f90bd8dbde11dc02040b1889614
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-04-16 13:24:56 +0200

    feat(nvim): add markview and live-preview for markdown previewing

---
+++ nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'OXY2DEV/markview.nvim',
+    lazy = false,
+    opts = {
+      preview = {
+        icon_provider = 'devicons',
+      },
+    },
+  },
+  {
+    'brianhuster/live-preview.nvim',
+    cmd = 'LivePreview',
+  },
+}
