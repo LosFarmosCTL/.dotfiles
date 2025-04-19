4a7fd05cb20c7f69d9808a003d99563ec392eac5
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-04-19 14:45:18 +0200

    fix(nvim): disable skipInsignificantPunctuation in nvim-spider

--- nvim/lua/plugins/spider.lua
+++ nvim/lua/plugins/spider.lua
@@ -6,5 +6,8 @@ return {
       { 'e', "<cmd>lua require('spider').motion('e')<CR>", mode = { 'n', 'o', 'x' } },
       { 'b', "<cmd>lua require('spider').motion('b')<CR>", mode = { 'n', 'o', 'x' } },
     },
+    opts = {
+      skipInsignificantPunctuation = false,
+    },
   },
 }
