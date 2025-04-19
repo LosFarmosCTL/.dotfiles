09b35597a499cfe548ea774831b8e7e3308e8c1a
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-04-19 21:04:21 +0200

    feat(nvim): make certain filetypes closeable by using `q`

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -11,6 +11,31 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- close certain filetypes with q -- idea taken from folke/LazyVim
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup 'close-filetypes-with-q',
+  pattern = {
+    'grug-far',
+    'neotest-summary',
+    'neotest-output',
+    'neotest-output-panel',
+    'help',
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.schedule(function()
+      vim.keymap.set('n', 'q', function()
+        vim.cmd 'close'
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, {
+        buffer = event.buf,
+        silent = true,
+        desc = 'Quit buffer',
+      })
+    end)
+  end,
+})
+
 -- auto rooter
 vim.g.cwd_before_auto_root = vim.uv.cwd()
 local auto_root_group = augroup 'auto-root'
