24758c782514926615c42fd9a8a2a3c045ac416d
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-04-25 16:29:41 +0200

    fix(nvim): ensure all LSPs are installed via mason automatically

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -113,7 +113,7 @@ return {
             vim.lsp.enable(server_name)
           end,
         },
-        ensure_installed = {},
+        ensure_installed = vim.tbl_keys(servers),
         automatic_installation = true,
       }
 
