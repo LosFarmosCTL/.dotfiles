a638b3e81659958c198c23a4759c91308de143df
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-05-06 16:17:14 +0200

    fix(nvim): don't use nvim-colorizer for tailwind classes

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -3,13 +3,12 @@ return {
     'catgoose/nvim-colorizer.lua',
     event = 'BufReadPre',
     opts = {
+      filetypes = {
+        'css',
+      },
       user_default_options = {
         css = true,
         css_fn = true,
-        tailwind = 'both',
-        tailwind_opts = {
-          update_names = true,
-        },
       },
     },
   },
