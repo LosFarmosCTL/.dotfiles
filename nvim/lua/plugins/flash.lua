2cda6fccd5d842d6ad41a35a18344e33db934cfd
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-06-27 13:43:27 +0200

    feat(nvim): add `flash.nvim`

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "m", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "M", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+    config = function(opts)
+      require('flash').setup(opts)
+
+      vim.keymap.set('n', '<leader>m', 'm', { desc = 'Set [M]ark' })
+    end,
+  },
+}
