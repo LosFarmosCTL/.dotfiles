57c8eda95b23967bf109163e7cdc2ea4289271c0
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-09-16 02:24:46 +0200

    feat(git): use difftastic for diff generation

--- git/aliases.fish
+++ git/aliases.fish
@@ -5,6 +5,8 @@ abbr -ag gl git log --graph --oneline
 abbr -ag gll git log --graph
 abbr -ag gd git diff
 abbr -ag gds git diff --staged
+abbr -ag gdd git diff --no-ext-diff
+abbr -ag gdds git diff --no-ext-diff --staged
 
 abbr -ag ga git add
 abbr -ag gc git commit
