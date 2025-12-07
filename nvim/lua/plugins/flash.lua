f3b2326743c2627ea7a8d933ceda6168b9a924f0
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-12-07 20:03:38 +0100

    fix: properly set up flash.nvim

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -11,7 +11,7 @@ return {
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
-    config = function(opts)
+    config = function(_, opts)
       require('flash').setup(opts)
 
       vim.keymap.set('n', '<leader>m', 'm', { desc = 'Set [M]ark' })
