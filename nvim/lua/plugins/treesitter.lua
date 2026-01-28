d752842205d4f680b61d75236fe06fe4adbc471a
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2026-01-28 05:57:13 +0100

    refactor(nvim): update nvim-treesitter to the new rewrite

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,77 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    event = { 'BufReadPost', 'BufWritePost', 'BufNewFile', 'VeryLazy' },
     lazy = false,
-    branch = 'master',
     build = ':TSUpdate',
-    opts = {
-      ensure_installed = {},
-      auto_install = true,
-      highlight = { enable = true, disable = { 'dockerfile' } },
-      indent = { enable = true },
-    },
-    config = function(_, opts)
-      require('nvim-treesitter.configs').setup(opts)
+    config = function()
+      local ts = require 'nvim-treesitter'
+
+      -- stylua: ignore
+      ts.install {
+        -- Core Neovim
+        'lua', 'vim', 'vimdoc', 'query', 'regex', 'luadoc', 'luap',
+        -- Web
+        'javascript', 'typescript', 'tsx', 'html', 'css', 'scss',
+        'json', 'json5', 'yaml', 'graphql', 'astro', 'vue', 'svelte',
+        -- Systems
+        'c', 'cpp', 'rust', 'go', 'zig',
+        -- Apple/Mobile
+        'swift', 'objc', 'kotlin',
+        -- Scripting
+        'python', 'ruby', 'bash', 'fish',
+        -- Config files
+        'toml', 'ini', 'dockerfile', 'gitignore', 'gitcommit',
+        'git_config', 'ssh_config', 'tmux', 'make',
+        -- Data/Docs
+        'markdown', 'markdown_inline', 'xml', 'sql', 'diff', 'jsdoc', 'comment',
+        -- Other
+        'java',
+      }
+
+      local function enable_treesitter(buf)
+        if pcall(vim.treesitter.start, buf) then
+          vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      end
+
+      local attempted = {}
+      vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('treesitter-auto-install', { clear = true }),
+        callback = function(args)
+          local ft = args.match
+          local buf = args.buf
+
+          -- NOTE: disable treesitter for dockerfiles,
+          -- there are some weird issues with the parser
+          if ft == '' or ft == 'dockerfile' then
+            return
+          end
+
+          local lang = vim.treesitter.language.get_lang(ft) or ft
+
+          -- check if parser is already installed
+          if attempted[lang] or vim.list_contains(ts.get_installed(), lang) then
+            enable_treesitter(buf)
+            return
+          end
+          attempted[lang] = true
+
+          -- install parser if not installed
+          if vim.list_contains(ts.get_available(), lang) then
+            vim.notify('Installing treesitter parser: ' .. lang, vim.log.levels.INFO)
+
+            ts.install(lang):await(vim.schedule_wrap(function(err)
+              if err then
+                vim.notify('Failed to install parser: ' .. lang .. ' - ' .. tostring(err), vim.log.levels.ERROR)
+                return
+              end
+
+              enable_treesitter(buf)
+              vim.notify('Treesitter ready: ' .. lang, vim.log.levels.INFO)
+            end))
+          end
+        end,
+      })
     end,
     -- NOTE: not technically a dependency, ensures markview.nvim is loaded before treesitter
     dependencies = { 'OXY2DEV/markview.nvim' },
