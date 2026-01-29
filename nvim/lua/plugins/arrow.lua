9f5bf348204f7b1f282274a028bd740781363ce7
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2026-01-29 23:43:17 +0100

    feat(nvim): add arrow.nvim

---
+++ nvim/lua/plugins/arrow.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    'otavioschwanck/arrow.nvim',
+    dependencies = {
+      { 'nvim-tree/nvim-web-devicons' },
+    },
+    opts = {
+      show_icons = true,
+      leader_key = ':',
+      buffer_leader_key = 'gm',
+      separate_by_branch = true,
+      separate_save_and_remove = true,
+      window = {
+        border = 'rounded',
+      },
+    },
+    -- stylua: ignore
+    keys = {
+      { ':', desc = 'Open Arrow bookmarks' },
+      { 'gm', desc = 'Open buffer bookmarks (Arrow)', mode = 'n' },
+      { '[a', function() require('arrow.persist').previous() end, desc = 'Previous Arrow bookmark' },
+      { ']a', function() require('arrow.persist').next() end, desc = 'Next Arrow bookmark' },
+      { '<leader>bm', function() require('arrow.persist').toggle() end, desc = '[b]uffer [m]ark in arrow.nvim' },
+    },
+  },
+}
