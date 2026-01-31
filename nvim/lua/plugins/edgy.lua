5c3580e075bd5e78a7f6c4fbbf910d3e6d0ff391
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2026-01-31 01:17:17 +0100

    fix(nvim): disable sidebar animation for search & replace

--- nvim/lua/plugins/edgy.lua
+++ nvim/lua/plugins/edgy.lua
@@ -3,10 +3,14 @@ return {
     'folke/edgy.nvim',
     event = 'VeryLazy',
     opts = {
+      animate = {
+        enabled = false,
+      },
       right = {
         {
           ft = 'grug-far',
           size = { width = 0.3 },
+          title = 'Search & Replace',
         },
       },
     },
