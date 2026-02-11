ce2b093e7f945e23837f7bc09295fa4d7fd9c01c
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2026-02-11 02:00:15 +0100

    feat(nvim): include arrow bookmark status in bufferline

--- nvim/lua/plugins/incline.lua
+++ nvim/lua/plugins/incline.lua
@@ -1,8 +1,9 @@
+-- TODO: toggle to enable/disable
 return {
   {
     'b0o/incline.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    event = 'VeryLazy',
+    event = { 'BufReadPost', 'VeryLazy' },
     opts = {
       window = {
         padding = 0,
@@ -12,18 +13,22 @@ return {
         local helpers = require 'incline.helpers'
         local devicons = require 'nvim-web-devicons'
 
-        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+        local bufname = vim.api.nvim_buf_get_name(props.buf)
+        local filename = vim.fn.fnamemodify(bufname, ':t')
         if filename == '' then
           filename = '[No Name]'
         end
 
-        local ft_icon, ft_color = devicons.get_icon_color(filename)
         local modified = vim.bo[props.buf].modified
+        local bookmarked = require('arrow.statusline').is_on_arrow_file(props.buf)
+
+        local ft_icon, ft_color = devicons.get_icon_color(filename)
         return {
           ft_icon and { ' ', ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or '',
           ' ',
           { filename, gui = modified and 'bold,italic' or 'bold' },
           ' ',
+          bookmarked and { ' ' } or '',
         }
       end,
     },
