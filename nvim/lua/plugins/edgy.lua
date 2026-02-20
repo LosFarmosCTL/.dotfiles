1c0b24e6d1931a75824f7e7c74a39c0eb9685c2e
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2026-02-20 17:50:33 +0100

    feat(nvim): add help entries and the neotest summary to edgy.nvim

--- nvim/lua/plugins/edgy.lua
+++ nvim/lua/plugins/edgy.lua
@@ -6,6 +6,19 @@ return {
       animate = {
         enabled = false,
       },
+      bottom = {
+        {
+          ft = 'help',
+          size = { height = 20 },
+          -- don't open help files in edgy that are being edited
+          filter = function(buf)
+            return vim.bo[buf].buftype == 'help'
+          end,
+        },
+      },
+      left = {
+        { title = 'Neotest Summary', ft = 'neotest-summary' },
+      },
       right = {
         {
           ft = 'grug-far',
