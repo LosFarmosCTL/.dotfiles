7c70257de86744bf8e30ca8c11ae8cd982bae1c4
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2026-03-27 02:10:50 +0100

    feat(opencode): add `??` fish function to generate annoying-to-write shell commands (ffmpeg, etc.) on the fly

--- opencode/aliases.fish
+++ opencode/aliases.fish
@@ -1,1 +1,19 @@
 abbr -ag oc opencode
+
+# @fish-lsp-disable-next-line 4004
+function ??
+  set -l prompt "You are a terminal assistant.
+  If needed for the task, you are allowed to explore the current directory to gather relevant context,
+  then deliver ONE single shell command that the user can run to solve their request.
+
+  CRITICAL - you should use pbcopy via the shell tool to store the generated command in the clipboard, for example: echo \"<command>\" | pbcopy
+  [optional: include 1-3 line explanation if flags are obscure, don't include lengthy explanations about tradeoffs/alternatives]
+
+  - Make sure to escape everything properly inside of the echoed string
+  - Do not output markdown formatting, stick to plain text
+
+  This is the users request: $argv
+  "
+
+  opencode run -m opencode/big-pickle "$prompt"
+end
